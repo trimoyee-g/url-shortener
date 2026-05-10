79cb29e07022d5f7188dbf8494e6bf7cde68812c
Trimoyee Ghosh <trimoyeeg@gmail.com>
2026-05-10 22:50:56 +0530

    feat: add validation constraints for auth request DTOs

--- src/main/java/com/urlshortener/url_shortener/dto/LoginRequest.java
+++ src/main/java/com/urlshortener/url_shortener/dto/LoginRequest.java
@@ -2,9 +2,15 @@ package com.urlshortener.url_shortener.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginRequest {
     @NotBlank @Email
     private String email;
--- src/main/java/com/urlshortener/url_shortener/dto/RegisterRequest.java
+++ src/main/java/com/urlshortener/url_shortener/dto/RegisterRequest.java
@@ -3,9 +3,15 @@ package com.urlshortener.url_shortener.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class RegisterRequest {
     @NotBlank @Email
     private String email;
